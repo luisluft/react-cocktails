fd6df41c46bec110d7ae87cafdaf889eff8d1735
luisluft <luberluft@gmail.com>
2021-10-04 07:44:03 -0400

    implement about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde minus
+        magnam ut officiis. Ex tempora, fuga aspernatur, dicta qui veniam
+        deleniti commodi, consequatur ipsam neque assumenda maxime maiores vitae
+        vel!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
